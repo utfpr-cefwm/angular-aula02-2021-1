d0be58ccddcc1381d19eeca4191757e64796fddb
Rui Leite <ruipimentelleite@gmail.com>
2022-05-15 20:34:34 +0000

    Exibe os botões de seleção de turma, com `AsyncPipe`.

--- src/app/components/lista-turmas/lista-turmas.component.ts
+++ src/app/components/lista-turmas/lista-turmas.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { Turma } from 'src/app/models/turma';
+import { TurmaService } from 'src/app/services/turma.service';
+
 @Component({
   selector: 'app-lista-turmas',
   templateUrl: './lista-turmas.component.html',
@@ -7,7 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaTurmasComponent implements OnInit {
 
-  constructor() { }
+  public turmas$: Observable<Turma[]> = this.turmaService.get().pipe(
+    tap(turmas => {
+      console.log('Chegaram as turmas!', turmas);
+    }),
+  );
+
+  constructor(
+    private turmaService: TurmaService,
+  ) {
+  }
 
   ngOnInit(): void {
   }
