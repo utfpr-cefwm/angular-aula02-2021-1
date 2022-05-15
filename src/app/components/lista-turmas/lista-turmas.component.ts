ceac87d86a420ebfae1338f98cb2a355c32e8e7f
Rui Leite <ruipimentelleite@gmail.com>
2022-05-15 20:14:06 +0000

    Obtém a lista de turmas do service, porém com gerenciamento manual da inscrição no `Observable`.

--- src/app/components/lista-turmas/lista-turmas.component.ts
+++ src/app/components/lista-turmas/lista-turmas.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { Turma } from 'src/app/models/turma';
+import { TurmaService } from 'src/app/services/turma.service';
 
 @Component({
   selector: 'app-lista-turmas',
   templateUrl: './lista-turmas.component.html',
   styleUrls: ['./lista-turmas.component.css']
 })
-export class ListaTurmasComponent implements OnInit {
+export class ListaTurmasComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public turmas?: Turma[];
+
+  private subOnDestroy: Subject<void> = new Subject();
+
+  constructor(
+    private turmaService: TurmaService,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.turmaService.get().pipe(
+      takeUntil(this.subOnDestroy),
+    ).subscribe(
+      (turmas: Turma[]) => {
+        this.turmas = turmas;
+        console.log('Chegaram as turmas!', turmas);
+      },
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subOnDestroy.next();
+    this.subOnDestroy.complete();
   }
 
 }
